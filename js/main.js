cf121ebaf41ae0bf47c3f48f924fb7fa0b5f0f74
Laura <lauracleave29@gmail.com>
2016-03-09 00:26:11 +1100

    Final-Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
 
+    $('input[type="submit"]').click(function (event) {
+
+        // Stop the default form action from executing
+        event.preventDefault();
+
+        // Get the value of text input fields
+        var incomplete = $('input[type="text"]').val();
+
+        // Check if nothing was enetered using the variable
+        if (incomplete == "") {
+
+            // Show alert 
+            alert("Incomplete text fields");
+        } else {
+            $('input[type="submit"]').submit();
+            window.location = "thankyou.html";
+        }
+    });
+
 });
